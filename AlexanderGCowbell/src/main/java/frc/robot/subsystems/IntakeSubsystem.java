d211521ff3e02e40ae4337c523226fa621eabfaa
wade <wcullison@gmail.com>
2024-01-11 21:10:56 -0500

    init IntakeSubsystem

--- AlexanderGCowbell/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ AlexanderGCowbell/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,30 @@
 package frc.robot.subsystems;
 
-public class IntakeSubsystem {
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.state.arm.ArmStateMachine;
+
+public class IntakeSubsystem  extends SubsystemBase {
+    private CANSparkMax intakeMotor;
     
+    public IntakeSubsystem() {
+        System.out.println("IntakeSubsystem: Starting up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+
+    private void initializeIntakeMotor() {
+        System.out.println("IntakeSubsystem: Initializing arm motors!!!!!!!!!!!!!!!!!!!!!!!!!");
+        intakeMotor = new CANSparkMax(ArmConstants.intakeCancoderId, CANSparkLowLevel.MotorType.kBrushless);
+        intakeMotor.restoreFactoryDefaults();
+        intakeMotor.setSmartCurrentLimit(ArmConstants.INTAKE_CURRENT_LIMIT_A);
+        intakeMotor.setInverted(false);
+        intakeMotor.setIdleMode(IdleMode.kBrake);
+    }
 }
