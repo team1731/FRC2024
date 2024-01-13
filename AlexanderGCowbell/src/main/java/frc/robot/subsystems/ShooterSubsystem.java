3bf388b141c38610d441c01116a3e1e1c2d61a72
david <davidanandsamy@aol.com>
2024-01-13 16:43:52 -0500

    Removed  current limits

--- AlexanderGCowbell/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ AlexanderGCowbell/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShooterSubsystem extends SubsystemBase{
 
     private CANSparkMax shooterMotor;
+    private CANSparkMax shooterMotor2;
 
     public ShooterSubsystem() {
         System.out.println("ShooterSubsystem: Starting up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
@@ -24,10 +25,15 @@ public class ShooterSubsystem extends SubsystemBase{
     private void initializeShooterMotor() {
         System.out.println("ShooterSubsystem: Initializing arm motors!!!!!!!!!!!!!!!!!!!!!!!!!");
         shooterMotor = new CANSparkMax(1, MotorType.kBrushless);
+        shooterMotor2 = new CANSparkMax(2, MotorType.kBrushless);
         shooterMotor.restoreFactoryDefaults();
+        shooterMotor2.restoreFactoryDefaults();
         shooterMotor.setSmartCurrentLimit(ArmConstants.INTAKE_CURRENT_LIMIT_A);
+        shooterMotor2.setSmartCurrentLimit(ArmConstants.INTAKE_CURRENT_LIMIT_A);
         shooterMotor.setInverted(false);
+        shooterMotor2.setInverted(false);
         shooterMotor.setIdleMode(IdleMode.kBrake);
+        shooterMotor2.setIdleMode(IdleMode.kBrake);
     }
 
     // public void reverseIntake() {
@@ -39,22 +45,25 @@ public class ShooterSubsystem extends SubsystemBase{
 
 
     public void shoot(){
-        double shootSpeed = 0.75;
-        shootSpeed = -1 * ArmConstants.cubeIntakeSpeed;
+        double shootSpeed = 1;
+        //shootSpeed = -1 * ArmConstants.cubeIntakeSpeed;
         System.out.println("ShooterSubsystem: speed = " + shootSpeed);
         shoot(shootSpeed);
 
     }
 
     public void shoot(double shooterSpeed) {
-        shooterMotor.setSmartCurrentLimit(ArmConstants.INTAKE_CURRENT_LIMIT_A);
+        //shooterMotor.setSmartCurrentLimit(ArmConstants.INTAKE_CURRENT_LIMIT_A);
         shooterMotor.set(shooterSpeed);
+        shooterMotor2.set(-shooterSpeed);
+        System.out.println(-shooterSpeed);
     }
 
     public void eject() {
-        shooterMotor.setSmartCurrentLimit(ArmConstants.EJECT_CURRENT_LIMIT);
+        //shooterMotor.setSmartCurrentLimit(ArmConstants.EJECT_CURRENT_LIMIT);
         // shooterMotor.set((stateMachine.getGamePiece() == GamePiece.CONE)? -1.0 : 1.0);
         shooterMotor.set(-1.0);
+        shooterMotor2.set(1);
     }
 
     public void holdShooting() {
@@ -64,8 +73,9 @@ public class ShooterSubsystem extends SubsystemBase{
     }
 
     public void stopShooting() {
-        shooterMotor.setSmartCurrentLimit(ArmConstants.INTAKE_CURRENT_LIMIT_A);
+        //shooterMotor.setSmartCurrentLimit(ArmConstants.INTAKE_CURRENT_LIMIT_A);
         shooterMotor.set(0);
+        shooterMotor2.set(0);
     }
     public boolean isIntakeAtStartedVelocity() {
         return (Math.abs(shooterMotor.getEncoder().getVelocity()) > ArmConstants.intakeStartedVelocityThreshold);
