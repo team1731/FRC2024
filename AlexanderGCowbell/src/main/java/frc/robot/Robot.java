6bff16ce9b2b796cef68f6021f3f9c328af60d9c
wade <wcullison@gmail.com>
2024-01-30 17:45:49 -0500

    merged wrist

--- AlexanderGCowbell/src/main/java/frc/robot/Robot.java
+++ AlexanderGCowbell/src/main/java/frc/robot/Robot.java
@@ -125,7 +125,7 @@ public class Robot extends TimedRobot {
 	m_ledstring = new LEDStringSubsystem(true);
 	intake_subsystem = new IntakeSubsystem(false);
 	elevatorSubsystem = new ElevatorSubsystem(false);
-	wristSubsystem = new WristSubsystem(true);
+	wristSubsystem = new WristSubsystem(false);
 
 	// Instantiate our robot container. This will perform all of our button bindings,
 	// and put our autonomous chooser on the dashboard
@@ -446,7 +446,7 @@ public class Robot extends TimedRobot {
 		//m_ledstring.setColor(LedOption.INIT);
     	if(doSD()){
 			System.out.println("TELEOP PERIODIC");
-			wristSubsystem.printPosition();
+			// wristSubsystem.printPosition();
 		}
 		
     	String newKeypadCommand = SmartDashboard.getString("keypadCommand", currentKeypadCommand);
