cb5e451e49c7279b1e7cc8cd81859e2ff4469e2b
wade <wcullison@gmail.com>
2024-02-12 20:12:14 -0500

    hooked up pathplanner

--- LowRider/src/main/java/frc/robot/CommandSwerveDrivetrain.java
+++ LowRider/src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -7,7 +7,13 @@ import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -27,6 +33,7 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Togglea
     // private Notifier m_simNotifier = null;
     // private double m_lastSimTime;
     private boolean enabled;
+    private final SwerveRequest.ApplyChassisSpeeds autoRequest = new SwerveRequest.ApplyChassisSpeeds();
 
     @Override
     public boolean isEnabled() {
@@ -42,29 +49,39 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Togglea
     public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
         //if (enabled){
             super(driveTrainConstants, modules);
+            configurePathPlanner();
         //}
-        // if (Utils.isSimulation()) {
-        //     startSimThread();
-        // }
+    }
+
+    private void configurePathPlanner() {
+        double driveBaseRadius = 0;
+        for (var moduleLocation : m_moduleLocations) {
+            driveBaseRadius = Math.max(driveBaseRadius, moduleLocation.getNorm());
+        }
+
+        AutoBuilder.configureHolonomic(
+            ()->this.getState().Pose, // Supplier of current robot pose
+            this::seedFieldRelative,  // Consumer for seeding pose against auto
+            this::getCurrentRobotChassisSpeeds,
+            (speeds)->this.setControl(autoRequest.withSpeeds(speeds)), // Consumer of ChassisSpeeds to drive the robot
+            new HolonomicPathFollowerConfig(new PIDConstants(10, 0, 0),
+                                            new PIDConstants(10, 0, 0),
+                                            TunerConstants.kSpeedAt12VoltsMps,
+                                            driveBaseRadius,
+                                            new ReplanningConfig()),
+            ()->false, // Change this if the path needs to be flipped on red vs blue
+            this); // Subsystem for requirements
     }
 
     public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
         return run(() -> this.setControl(requestSupplier.get()));
     }
 
-    // private void startSimThread() {
-    //     m_lastSimTime = Utils.getCurrentTimeSeconds();
+    public Command getAutoPath(String pathName) {
+        return new PathPlannerAuto(pathName);
+    }
 
-    //     /* Run simulation at a faster rate so PID gains behave more reasonably */
-    //     m_simNotifier = new Notifier(() -> {
-    //         final double currentTime = Utils.getCurrentTimeSeconds();
-    //         double deltaTime = currentTime - m_lastSimTime;
-    //         m_lastSimTime = currentTime;
-
-    //         /* use the measured time delta, get battery voltage from WPILib */
-    //         updateSimState(deltaTime, RobotController.getBatteryVoltage());
-    //     });
-    //     m_simNotifier.startPeriodic(kSimLoopPeriod);
-    // }
-
+    public ChassisSpeeds getCurrentRobotChassisSpeeds() {
+        return m_kinematics.toChassisSpeeds(getState().ModuleStates);
+    }
 }
