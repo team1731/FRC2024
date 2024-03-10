d1b06990d8197b20a584cc29554ddbc0e6e77d10
pclifesavers <pclifesavers@gmail.com>
2024-03-10 18:33:08 -0400

    prefer configuring path on the fly vs. canned path files

--- LowRider/src/main/java/frc/robot/commands/DriveToTrapCommand.java
+++ LowRider/src/main/java/frc/robot/commands/DriveToTrapCommand.java
@@ -22,16 +22,41 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.CommandSwerveDrivetrain;
 
-/**
- * Command to fire into the speaker
- */
+class Pose {
+	double x, y, degrees;
+	public Pose(double x, double y, double degrees){
+		this.x = x; this.y = y; this.degrees = degrees;
+	}
+	public Pose2d toPose2d(){
+		return new Pose2d(x, y, new Rotation2d(Units.degreesToRadians(degrees)));
+	}
+}
+
+class PosePair {
+	Pose start, end;
+	public PosePair(Pose start, Pose end){
+		this.start = start; this.end = end;
+	}
+	public Pose2d[] toPose2dArray(){
+		return new Pose2d[]{start.toPose2d(), end.toPose2d()};
+	}
+}
+
 public class DriveToTrapCommand extends Command {
 	@SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 	private final CommandSwerveDrivetrain m_drivetrain;
-	private Pose2d endPos;
+	private Pose2d endPose;
 	private Command pathFollowingCommand;
 
-
+	private PosePair[] STAGE_POSES = {
+        //                                      STARTING POSE                ENDING POSE
+		/* BLUE LEFT   */ new PosePair(new Pose( 3.84, 5.81,  -60), new Pose( 4.43, 4.85,  -60)),
+		/* BLUE RIGHT  */ new PosePair(new Pose( 4.12, 2.79,   60), new Pose( 4.40, 3.30,   60)),
+		/* BLUE CENTER */ new PosePair(new Pose( 6.48, 4.17,  180), new Pose( 5.38, 4.17,  180)),
+		/* RED LEFT    */ new PosePair(new Pose(12.65, 2.46,  120), new Pose(12.17, 3.33,  120)),
+		/* RED RIGHT   */ new PosePair(new Pose(12.55, 5.75, -120), new Pose(12.08, 4.88, -120)),
+		/* RED CENTER  */ new PosePair(new Pose( 9.59, 4.08,    0), new Pose(10.78, 4.08,    0))
+	};
 
 
 	/**
@@ -53,9 +78,27 @@ public class DriveToTrapCommand extends Command {
 	// If it is used as Default command then it gets call all the time
 	@Override
 	public void initialize() {
+	  //
+	  // It may be desirable to use a "canned" path approach (left here for possible future consideration):
+	  // PathPlannerPath path = PathPlannerPath.fromPathFile("paths/Chain.path");
+	  //
 
-
-	  PathPlannerPath path = PathPlannerPath.fromPathFile("paths/Chain.path");
+	  Pose2d currentPose = m_drivetrain.getState().Pose;
+	  Pose2d[] closestPoses = getClosestChainPoses(currentPose);
+	  endPose = closestPoses[0];
+      
+      // The rotation component in these poses represents the direction of travel
+      Pose2d startPos = new Pose2d(currentPose.getTranslation(), new Rotation2d());
+      
+      List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(startPos, endPose);
+      PathPlannerPath path = new PathPlannerPath(
+        bezierPoints, 
+        new PathConstraints(
+          4.0, 4.0, 
+          Units.degreesToRadians(360), Units.degreesToRadians(540)
+        ),  
+        new GoalEndState(0.0, currentPose.getRotation())
+      );
 
       // Prevent this path from being flipped on the red alliance, since the given positions are already correct
       path.preventFlipping = true;
@@ -67,10 +110,19 @@ public class DriveToTrapCommand extends Command {
   
 
 	
-
-	private Pose2d getClosestChainPose(Pose2d currentPose) {
-         // this is just returning a pose 2 meters to the left of the current pose - needs to be 
-		 return new Pose2d(currentPose.getTranslation().plus(new Translation2d(2.0, 0.0)), new Rotation2d());
+	
+	private Pose2d[] getClosestChainPoses(Pose2d currentPose) {
+		Pose2d[] closestPoses = null;
+		double shortestDistance = 100;
+		for (PosePair posePair : STAGE_POSES) {
+			Pose2d[] fieldPoses = posePair.toPose2dArray();
+			double distanceToCurrent = currentPose.getTranslation().getDistance(fieldPoses[0].getTranslation());
+			if(distanceToCurrent < shortestDistance){
+				shortestDistance = distanceToCurrent;
+				closestPoses = fieldPoses;
+			}
+		}
+		return closestPoses;
 	}
 
 	// Called every time the scheduler runs while the command is scheduled.
