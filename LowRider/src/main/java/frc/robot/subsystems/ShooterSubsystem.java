69bbc8e41cc5b96159b80da0b5ce718ecef13102
wade <wcullison@gmail.com>
2024-03-25 21:26:37 -0400

    test code for newShooter

--- LowRider/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ LowRider/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,27 +5,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
+// import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.Constants;
-import frc.robot.Constants.ShooterConstants;
+// import frc.robot.Constants.ShooterConstants;
 import frc.robot.Robot;
 
 public class ShooterSubsystem extends SubsystemBase implements ToggleableSubsystem{
 
-  //  private CANSparkMax shooterMotor1;
- //   private CANSparkMax shooterMotor2;
-  //  private SparkPIDController shooterPIDController1;
- //   private SparkPIDController shooterPIDController2;
     private static final String canBusName = Constants.CANBUS_NAME;
     private final TalonFX m_fx = new TalonFX(Constants.ShooterConstants.shooterCancoderId1, canBusName);
     private final TalonFX m_fllr = new TalonFX(Constants.ShooterConstants.shooterCancoderId2, canBusName);
     private final VelocityVoltage m_voltageVelocity = new VelocityVoltage(0, 0, false, 0, 0, false, false, false);
     private final NeutralOut m_brake = new NeutralOut();
     private boolean enabled;
-    private boolean isShooting = false;
+    // private boolean isShooting = false;
     @Override
     public boolean isEnabled() {
         return enabled;
@@ -34,6 +30,7 @@ public class ShooterSubsystem extends SubsystemBase implements ToggleableSubsyst
     public ShooterSubsystem(boolean enabled) {
         this.enabled = enabled;
         initializeShooterMotor();
+        SmartDashboard.putNumber("shooterDiff", 0);
     }
 
     private void initializeShooterMotor() {
@@ -50,41 +47,51 @@ public class ShooterSubsystem extends SubsystemBase implements ToggleableSubsyst
             configs.Voltage.PeakForwardVoltage = 12;
             configs.Voltage.PeakReverseVoltage = -12;
 
-                /* Retry config apply up to 5 times, report if failure */
+            /* Retry config apply up to 5 times, report if failure */
             StatusCode status = StatusCode.StatusCodeNotInitialized;
             for (int i = 0; i < 5; ++i) {
-            status = m_fx.getConfigurator().apply(configs);
-            if (status.isOK()) break;
+                status = m_fx.getConfigurator().apply(configs);
+                if (status.isOK()) break;
             }
             if(!status.isOK()) {
-            System.out.println("Could not apply configs, error code: " + status.toString());
+                System.out.println("Could not apply configs, error code: " + status.toString());
             }
 
-                            /* Retry config apply up to 5 times, report if failure */
-           status = StatusCode.StatusCodeNotInitialized;
+            /* Retry config apply up to 5 times, report if failure */
+            status = StatusCode.StatusCodeNotInitialized;
             for (int i = 0; i < 5; ++i) {
-            status = m_fllr.getConfigurator().apply(configs);
-            if (status.isOK()) break;
+                status = m_fllr.getConfigurator().apply(configs);
+                if (status.isOK()) break;
             }
             if(!status.isOK()) {
-            System.out.println("Could not apply configs, error code: " + status.toString());
+                System.out.println("Could not apply configs, error code: " + status.toString());
             }
 
-           // m_fllr.setControl(new Follower(m_fx.getDeviceID(), false));
-            }
-                    
-        }
+            // m_fllr.setControl(new Follower(m_fx.getDeviceID(), false));
+        }           
+    }
 
     public void shoot(){
         if (enabled){
-            if (Robot.doSD()) { 
-                System.out.println("ShooterSubsystem: m1speed, m2speed = " + ShooterConstants.kMotorSpeed1 + ", " + ShooterConstants.kMotorSpeed2); 
+            double speedDiff = 0;
+            double m1speed = 90;
+            double m2speed = 90;
+            double speedDiff2 = SmartDashboard.getNumber("shooterDiff", speedDiff);
+            if (speedDiff2 >= 0 && speedDiff2 <= 10) {
+                m1speed += speedDiff2;
+                m2speed -= speedDiff2;
             }
-            isShooting = true;
-            m_fx.setControl(m_voltageVelocity.withVelocity(6000.0/60));
-            m_fllr.setControl(m_voltageVelocity.withVelocity(6000.0/60));
+            
+            // isShooting = true;
+            // Kraken freespeed: 6000 (gearing 24/18), Falcon 500 freespeed 6380
+            m_fx.setControl(m_voltageVelocity.withVelocity(m1speed)); // 6000/60
+            m_fllr.setControl(m_voltageVelocity.withVelocity(m2speed));   // 6000/60
+            if (Robot.doSD()) { 
+                // System.out.println("ShooterSubsystem: m1speed, m2speed = " + m1speed + ", " + m2speed); 
+                SmartDashboard.putNumber("m1Speed", m1speed); 
+                SmartDashboard.putNumber("m2Speed", m2speed); 
+            }
 		}  
-
     }
 
     public void shooterAsIntake(){
@@ -94,31 +101,26 @@ public class ShooterSubsystem extends SubsystemBase implements ToggleableSubsyst
         }     
     }
 
-        public void periodic() {
+    public void periodic() {
         if(!enabled) return;
         SmartDashboard.putNumber("shooter1 velocity", m_fx.getVelocity().getValueAsDouble());
         SmartDashboard.putNumber("shooter2 velocity", m_fllr.getVelocity().getValueAsDouble());
-
-
     }
 
     public void stopShooting() {
         if (enabled){
-            isShooting = false;
+            // isShooting = false;
             m_fx.setControl(m_brake);
             m_fllr.setControl(m_brake);
         }
     }
 
-
-
     public void shootAmp() {
             m_fx.setControl(m_voltageVelocity.withVelocity(1000.0/60));
             m_fllr.setControl(m_voltageVelocity.withVelocity(1000.0/60));
-
     }
-    public void reverseSlow() {
 
+    public void reverseSlow() {
             m_fx.setControl(m_voltageVelocity.withVelocity(-500.0/60));
             m_fllr.setControl(m_voltageVelocity.withVelocity(-500.0/60));
     }
@@ -126,9 +128,5 @@ public class ShooterSubsystem extends SubsystemBase implements ToggleableSubsyst
     public double getShooterVelocity() {
         return m_fx.getVelocity().getValueAsDouble();
     }
-
-
-
-
 }
 
